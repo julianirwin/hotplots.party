d979a8920dea833c636dcfd5976163eeb25bad51
Julian Irwin <julian.irwin@gmail.com>
2016-09-04 16:02:31 -0500

    added 404 to upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 aws s3 cp index.html s3://hotplots.party/index.html
+aws s3 cp 404.html s3://hotplots.party/404.html
 aws s3 cp --recursive bootstrap s3://hotplots.party/bootstrap
 aws s3 cp --recursive script s3://hotplots.party/script
 aws s3 cp --recursive css s3://hotplots.party/css
